b9cc854735b70c3e3ac0dc0d7b1ee2e8dfe91db6
Ap.Muthu <apmuthu@usa.net>
2014-12-19 00:13:57 +0530

    Added default_quote_valid_days into sys_prefs table to match Sales Quote Inquiry feature addition upstream http://frontaccounting.com/punbb/viewtopic.php?id=5393

--- FAMods/sql/en_US-new.sql
+++ FAMods/sql/en_US-new.sql
@@ -1830,6 +1830,7 @@ INSERT INTO `0_sys_prefs` VALUES ('version_id', 'system', 'varchar', 11, '2.3rc'
 INSERT INTO `0_sys_prefs` VALUES ('auto_curr_reval', 'setup.company', 'smallint', 6, '1');
 INSERT INTO `0_sys_prefs` VALUES ('grn_clearing_act', 'glsetup.purchase', 'varchar', 15, '1550');
 INSERT INTO `0_sys_prefs` VALUES ('bcc_email', 'setup.company', 'varchar', 100, '');
+INSERT INTO `0_sys_prefs` VALUES ('default_quote_valid_days', 'glsetup.sales', 'smallint', 6, 30);
 
 -- --------------------------------------------------------
 
