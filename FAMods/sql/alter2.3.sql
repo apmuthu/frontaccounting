2669386464c43fccbf3aa42d42ad95fa5212cdca
Ap.Muthu <apmuthu@usa.net>
2014-12-21 22:56:14 +0530

    FA CoA upgrade sqls for recent fixes https://github.com/apmuthu/frontaccounting/commit/b9cc854735b70c3e3ac0dc0d7b1ee2e8dfe91db6 https://github.com/apmuthu/frontaccounting/commit/be5ca3133d5b8d2d15ea50f41d05c52b1590ff49

--- FAMods/sql/alter2.3.sql
+++ FAMods/sql/alter2.3.sql
@@ -220,7 +220,7 @@ CREATE TABLE `0_crm_contacts` (
 
 
 #
-# tmp_id, tmp_class fields are used temporarily during upgrade to makethe process easier
+# tmp_id, tmp_class fields are used temporarily during upgrade to make the process easier
 #
 INSERT INTO `0_crm_persons` (`ref`, `email`, `lang`, `tmp_id`, `tmp_class`) 
 	SELECT `debtor_ref`, `email`, if(`curr_code`=d.`lang`, NULL, 'en_GB'), `debtor_no`, 'customer'
@@ -251,3 +251,8 @@ ALTER TABLE `0_supp_trans` ADD COLUMN `tax_included` tinyint(1) NOT NULL default
 ALTER TABLE `0_purch_orders` ADD COLUMN `tax_included` tinyint(1) NOT NULL default '0';
 ALTER TABLE `0_debtor_trans` ADD INDEX `order_` (`order_`);
 ALTER TABLE `0_users` CHANGE `query_size` `query_size` TINYINT(1) UNSIGNED NOT NULL DEFAULT 10;
+
+INSERT INTO `0_sys_prefs` VALUES ('default_quote_valid_days', 'glsetup.sales', 'smallint', 6, 30);
+ALTER TABLE `0_supp_trans` DROP INDEX `supplier_id`; 
+ALTER TABLE `0_supp_trans` DROP INDEX `type`; 
+ALTER TABLE `0_cust_branch` DROP INDEX `branch_code`; 
