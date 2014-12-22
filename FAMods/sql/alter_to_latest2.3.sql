d33395dc9255c8bf97abcfb05f039eca1767efa3
Ap.Muthu <apmuthu@usa.net>
2014-12-22 19:17:03 +0530

    Reversion of redundant indexes removal in core and backported it into FAMods to sync with upstream (minor version db schema change policy)

--- FAMods/sql/alter_to_latest2.3.sql
+++ FAMods/sql/alter_to_latest2.3.sql
@@ -1,9 +1,13 @@
 ALTER TABLE `0_debtor_trans` ADD INDEX `order_` (`order_`);
 ALTER TABLE `0_users` CHANGE `query_size` `query_size` TINYINT(1) UNSIGNED NOT NULL DEFAULT 10;
 
-INSERT INTO `0_sys_prefs` VALUES ('default_quote_valid_days', 'glsetup.sales', 'smallint', 6, 30);
-ALTER TABLE `0_supp_trans` DROP INDEX `supplier_id`; 
-ALTER TABLE `0_supp_trans` DROP INDEX `type`; 
-ALTER TABLE `0_cust_branch` DROP INDEX `branch_code`; 
+ALTER TABLE `0_crm_contacts` CHANGE `entity_id` INT(11) DEFAULT NULL COMMENT 'entity id in related class table';
+
+INSERT IGNORE INTO `0_sys_prefs` VALUES ('default_quote_valid_days', 'glsetup.sales', 'smallint', 6, 30);
+
+ALTER TABLE `0_cust_branch` DROP INDEX `branch_code`;
+ALTER TABLE `0_supp_trans` DROP INDEX `supplier_id`;
+ALTER TABLE `0_supp_trans` DROP INDEX `type`;
 ALTER TABLE `0_supp_trans` DROP INDEX `SupplierID_2`, ADD INDEX `supplier_id` (`supplier_id`, `supp_reference`);
-ALTER TABLE `0_stock_moves` CHANGE `stock_id` `stock_id` VARCHAR(20) DEFAULT '' NOT NULL; 
+
+ALTER TABLE `0_stock_moves` CHANGE `stock_id` `stock_id` VARCHAR(20) DEFAULT '' NOT NULL;
