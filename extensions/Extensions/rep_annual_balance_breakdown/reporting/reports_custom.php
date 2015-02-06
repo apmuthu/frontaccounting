75921fbda225b8417b8ee200495c983bdd61f23f
Ap.Muthu <apmuthu@usa.net>
2015-02-07 00:15:57 +0530

    Updated Annual balance and expense reports extensions to include dimension2 in the request form

--- extensions/Extensions/rep_annual_balance_breakdown/reporting/reports_custom.php
+++ extensions/Extensions/rep_annual_balance_breakdown/reporting/reports_custom.php
@@ -4,7 +4,8 @@ global $reports, $dim;
 
 $reports->addReport(RC_GL,"_annual_balance_breakdown",_('Annual &Balance Breakdown - Detailed'),
        array(  _('Report Period') => 'DATEENDM',
-                       _('Dimension') => 'DIMENSIONS1',
+                       _('Dimension')." 1" =>  'DIMENSIONS1',
+                       _('Dimension')." 2" =>  'DIMENSIONS2',
                        _('Comments') => 'TEXTBOX',
                        _('Destination') => 'DESTINATION'));
 ?>
--- extensions/Extensions/rep_annual_expense_breakdown/reporting/reports_custom.php
+++ extensions/Extensions/rep_annual_expense_breakdown/reporting/reports_custom.php
@@ -4,7 +4,8 @@ global $reports;
 
 $reports->addReport(RC_GL,"_annual_expense_breakdown",_('Annual &Expense Breakdown - Detailed'),
        array(  _('Report Period') => 'DATEENDM',
-                       _('Dimension') => 'DIMENSIONS1',
+                       _('Dimension')." 1" =>  'DIMENSIONS1',
+                       _('Dimension')." 2" =>  'DIMENSIONS2',
                        _('Comments') => 'TEXTBOX',
                        _('Destination') => 'DESTINATION'));
 ?>
